b9f01b068455b35e79d57fe085ca77565246822d
Nikita <niksaen.games.official@gmail.com>
2023-11-21 21:59:53 +0300

    Update

--- app/src/main/java/com/niksaen/tetrisliga/Activity1.kt
+++ app/src/main/java/com/niksaen/tetrisliga/Activity1.kt
@@ -10,8 +10,6 @@ import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-
-
 class Activity1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
